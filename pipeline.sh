74dc38823b50d0ca2b95f6411302677aef333b4b
Dominic <twhvyt@gmail.com>
2024-09-23 20:48:56 -0600

    Added cosmetics and sanity check

--- pipeline.sh
+++ pipeline.sh
@@ -14,11 +14,15 @@ chmod -R 755 wavs output
 sudo apt install -y python3 python3-venv python3-pip ffmpeg zip
 
 #reboot?
-echo "[OK] System updated. You may need to reboot if kernel updates were applied. Reboot now? (y/n)"
+echo
+echo -e "\e[35m=======================================================\e[0m"
+echo -e "[OK] \e[46mSystem updated. You may need to reboot if kernel updates were applied. Reboot now? (y/n)\e[0m"
 read reboot_choice
 if [ "$reboot_choice" = "y" ]; then
     sudo reboot
 fi
+echo -e "\e[35m=======================================================\e[0m"
+echo
 
 #venv setup
 python3 -m venv venv
@@ -28,11 +32,11 @@ source venv/bin/activate
 pip install -r requirements.txt
 echo
 echo
-echo "======================================================="
+echo -e "\e[35m=======================================================\e[0m"
 echo
-echo "Notice: Please place your .wav files in the 'wavs/' directory now"
+echo -e "\e[36mNotice: Please place your .wav files in the 'wavs/' directory now\e[0m"
 echo
-echo "======================================================="
+echo -e "\e[35m=======================================================\e[0m"
 echo
 echo "Do you wish to automatically clean up background noise? Please note this function is a little quirky. Use at your own risk. (y/n)"
 read filter_choice
@@ -42,7 +46,7 @@ else
     echo "[OK] You can run it manually later with: python3 filter.py"
 fi
 echo
-echo "======================================================="
+echo -e "\e[35m=======================================================\e[0m"
 echo
 echo "Do you wish to split the audio files into smaller chunks? It's recommended that you do so. (y/n)"
 read filter_choice
@@ -52,9 +56,9 @@ else
     echo "[OK] You can run it manually later with: python3 split.py"
 fi
 echo
-echo "======================================================="
+echo -e "\e[35m=======================================================\e[0m"
 echo
-echo "Ready to run the main.py script to generate the dataset? (y/n)"
+echo "Ready to run the generation script to generate the dataset? (y/n)"
 read run_choice
 if [ "$run_choice" = "y" ]; then
     python3 main.py
@@ -62,9 +66,18 @@ else
     echo "[OK] You can run it manually later with: python3 main.py"
 fi
 echo
-echo "======================================================="
+echo -e "\e[35m=======================================================\e[0m"
 echo
 #deactivate venv
 deactivate
 
-echo "[OK] Dataset Generation Complete! You should now see a zip folder located in output/ named dataset.zip"
+if [ -f "output/dataset.zip" ]; then
+    echo -e "[OK] \e[32mDataset Generation Complete! You should now see a zip folder located in output/ named dataset.zip\e[0m"
+else
+    echo -e "\e[31m=======================================================\e[0m"
+    echo -e "[ERROR] \e[31mDataset generation failed! The expected file output/dataset.zip does not exist.\e[0m"
+    echo
+    echo "[ERROR] \e[31mConsider making an issue thread at https://github.com/DominicTWHV/LJSpeech_Dataset_Generator/issues !\e[0m"
+    echo -e "\e[31m=======================================================\e[0m"
+fi
+
