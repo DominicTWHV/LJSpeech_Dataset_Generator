12b9ea3c473a5e6373f27bc06d7ec0d3d5eeacef
DominicTWHV <twhvyt@gmail.com>
2025-01-23 04:22:24 -0700

    Dataset.zip deletion support

--- functions/helper/janitor.py
+++ functions/helper/janitor.py
@@ -9,6 +9,8 @@ class Janitor:
         wavs_dir = os.path.join(root_dir, 'wavs')
         metadata_file = os.path.join(root_dir, 'metadata.csv')
 
+        dataset_file = os.path.join(root_dir, 'output/dataset.zip')
+
         wavs_deleted = 0
         if os.path.exists(wavs_dir):
             for filename in os.listdir(wavs_dir):
@@ -18,8 +20,22 @@ class Janitor:
                     wavs_deleted += 1
 
         metadata_deleted = False
+        dataset_deleted = False
+
         if os.path.exists(metadata_file):
             os.remove(metadata_file)
             metadata_deleted = True
+
+        if os.path.exists(dataset_file):
+            os.remove(dataset_file)
+            dataset_deleted = True
         
-        return f"Deleted {wavs_deleted} files from 'wavs' directory and metadata file: {'deleted' if metadata_deleted else 'not found'}.\nTarget Directories:\n{wavs_dir}\n{metadata_file}"
+        return (
+            f"Deleted {wavs_deleted} files from 'wavs' directory.\n"
+            f"Metadata file: {'deleted' if metadata_deleted else 'not found'}.\n"
+            f"Dataset file: {'deleted' if dataset_deleted else 'not found'}.\n\n"
+            f"Target Directories:\n"
+            f"  Wavs Directory: {wavs_dir}\n"
+            f"  Metadata File: {metadata_file}\n"
+            f"  Dataset File: {dataset_file}"
+        )
