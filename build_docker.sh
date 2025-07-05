563dd3c0eb19e202d82cf40d6bc34bb612772fbd
DominicTWHV <twhvyt@gmail.com>
2025-07-04 21:00:04 -0600

    Docker setup script

---
+++ build_docker.sh
@@ -0,0 +1,17 @@
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+if command -v docker &> /dev/null
+then
+    echo "Docker is already installed"
+else
+    echo "Docker could not be found, installing..."
+    
+    apt update
+    apt install -y docker.io
+fi
+
+docker build -t LJSpeech_DSG .
+
+docker run cockatoo
